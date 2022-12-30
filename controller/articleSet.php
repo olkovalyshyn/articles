d791ac35e6a39852ec6c5ff7b830465d9c695f36
Olexandr Kovalyshyn <kovalyshyn@gmail.com>
2022-12-31 01:37:01 +0200

    Add xDebug

--- controller/articleSet.php
+++ controller/articleSet.php
@@ -2,15 +2,20 @@
 include "../connect/connect.php";
 include "../model/articles/articleSet.php";
 
-$login = $_COOKIE['user'];
-$article = $_POST['article'];
-$article = htmlspecialchars($article);
+// $login = $_COOKIE['user'];
+// $article = $_POST['article'];
+// $article = htmlspecialchars($article);
 
 if (
     isset($_POST["submitAddArticle"]) &&
     isset($_COOKIE['user']) &&
     strlen($_POST['article']) > 0
 ) {
+
+    $login = $_COOKIE['user'];
+    $article = $_POST['article'];
+    $article = htmlspecialchars($article);
+
 
     $article = new ArticleSet($pdo);
     $result = $article->set($login, $article);
